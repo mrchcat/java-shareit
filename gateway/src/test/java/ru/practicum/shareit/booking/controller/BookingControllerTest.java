93e7592b37d37b2d222e8f29ba13c0e88a5572af
mrchcat <m-cat@mail.ru>
2024-08-25 09:56:43 +0300

    fix: checkstyle

--- gateway/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/controller/BookingControllerTest.java
@@ -9,16 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.booking.client.BookingClient;
-import ru.practicum.shareit.booking.dto.BookingCreateDto;
-
-import java.time.LocalDateTime;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(controllers = BookingController.class)
 class BookingControllerTest {
@@ -36,27 +26,6 @@ class BookingControllerTest {
     @Test
     @DisplayName("create new booking")
     void createBooking() throws Exception {
-//        String path = "/bookings";
-//        long userId = 1L;
-//        long itemId = 100L;
-//        LocalDateTime start = LocalDateTime.now();
-//        LocalDateTime end = start.plusDays(10);
-//        BookingCreateDto booking = BookingCreateDto.builder()
-//                .itemId(itemId)
-//                .start(start)
-//                .end(end)
-//                .build();
-//        String json = objectMapper.writeValueAsString(booking);
-//
-//        mockMvc.perform(
-//                 post(path)
-//                .contentType("application/json")
-//                .content(json)
-//                .header(userHeaderName,String.valueOf(userId))
-//        );
-//
-//        verify(bookingClient, times(1)).createBooking(any(),any());
-//        verifyNoMoreInteractions(bookingClient);
         Assertions.assertTrue(true);
     }
 
