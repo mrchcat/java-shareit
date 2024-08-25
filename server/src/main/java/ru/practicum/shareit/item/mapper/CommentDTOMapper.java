d4ccde4514ab71a41ed049db3c8ecd3b5fec3f1e
mrchcat <m-cat@mail.ru>
2024-08-25 10:38:07 +0300

    fix: checkstyle

--- server/src/main/java/ru/practicum/shareit/item/mapper/CommentDTOMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/CommentDTOMapper.java
@@ -6,8 +6,6 @@ import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDateTime;
-
 public class CommentDTOMapper {
     public static CommentDTO toDTO(Comment comment) {
         return CommentDTO.builder()
